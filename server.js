9f41f9ae7aacf47bee1d007f840dac4d77aa14f2
Guido García <guido@tid.es>
2015-01-02 21:37:18 +0100

    scaler down alpha

---
+++ server.js
@@ -0,0 +1,89 @@
+var Marathon = require('marathon.node'),
+    request = require('request'),
+    csv = require('csv-stream'),
+    _ = require('underscore');
+
+// TODO read from consul
+var MARATHON_API = 'http://10.141.141.10:8080';
+var HAPROXY_STATS_CSV = 'http://10.141.141.10:9090/;csv';
+var HAPROXY_USER = 'admin';
+var HAPROXY_PASS = 'admin';
+
+var client = new Marathon({base_url: MARATHON_API});
+
+var scaleDown = function scaleDown(appName, callback) {
+    client.app(appName).get().then(function onapp(res) {
+        var currentInstances = res.app.instances;
+        var instances = currentInstances > 1 ? currentInstances - 1 : 0;
+        client.app(appName).update({instances: instances}).then(function onscale(res) {
+            console.log(res);
+            callback();
+        });
+    });
+};
+
+var scaleUp = function scaleUp(appName, callback) {
+    client.app(appName).get().then(function onapp(res) {
+        var currentInstances = res.app.instances;
+        var instances = currentInstancs + 1;
+        client.app(appName).update({instances: instances}).then(function onscale(res) {
+            console.log(res);
+            callback();
+        });
+    });
+};
+
+var getHaproxyStats = function getHaproxyStats(callback) {
+    var stats = [];
+
+    var ondata = function ondata(data) {
+        stats.push(data);
+    };
+
+    var onerror = function onerror(err) {
+        callback(err);
+    };
+
+    var onend = function onend() {
+        var err;
+        callback(undefined, stats);
+    };
+
+    request.get({url:HAPROXY_STATS_CSV})
+        .auth(HAPROXY_USER, HAPROXY_PASS, false)
+        .pipe(csv.createStream())
+        .on('error', onerror)
+        .on('data', ondata)
+        .on('end', onend)
+};
+
+getHaproxyStats(function onstats(err, stats) {
+    var filterFlockInstances = function isNotBackupServer(row) {
+        return row['# pxname'].indexOf('flock-backup') === -1 &&
+               row['svname'].indexOf('server_backup') === -1 &&
+               row['svname'].indexOf('server_') === 0
+    };
+
+    var FIELDS = ['# pxname', 'lastsess', 'qcur', 'qmax', 'scur', 'smax', 'status', 'svname', 'qtime', 'ctime', 'rtime'];
+    var servers = _.chain(stats)
+        .filter(filterFlockInstances)
+        .map(function (row) { return _.pick(row, FIELDS) })
+        .value();
+
+    console.log('current servers', servers);
+
+    var filterIdleServers = function isIdle(server) {
+        return server.lastsess > 60;
+    };
+
+    var idleServers = _.filter(servers, filterIdleServers);
+    idleServers.forEach(function (server) {
+        var appId = '/' + server['# pxname'];
+        scaleDown(appId, function () {
+            // fire and forget
+            console.log('try to scale down app %s', appId);
+        });
+    });
+
+    // TODO use 'xxxxxxxx' to decide when to scale up
+});
